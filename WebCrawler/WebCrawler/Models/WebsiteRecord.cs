c9cc657ec2f5f3397d71066b70f62e37049163bd
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-13 20:12:32 +0200

    Add WebsiteRecord class

---
+++ WebCrawler/WebCrawler/Models/WebsiteRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace WebCrawler.Models {
+    public class WebsiteRecord {
+        public int Id { get; set; } 
+        public string Regex { get; set; }
+        public int Days { get; set; }
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public string Label { get; set; }
+    }
+}
