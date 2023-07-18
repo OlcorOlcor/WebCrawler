08423fa05f983fcfa6182ba98bd1959719a8ed68
Jan Simek <jan.simek.4242@gmail.com>
2023-07-18 16:15:48 +0200

    Add WebPage class.

---
+++ WebCrawler/WebCrawler/Models/Execution.cs
@@ -0,0 +1,16 @@
+﻿namespace WebCrawler.Models {
+    public class Execution {
+        private readonly string _url;
+        private readonly string _regex;
+
+        public Execution(string url, string regex) {
+            this._url = url;
+            this._regex = regex;
+        }
+
+        public void Execute() {
+            //TODO
+        }
+
+    }
+}
---
+++ WebCrawler/WebCrawler/Models/WebPage.cs
@@ -0,0 +1,15 @@
+﻿namespace WebCrawler.Models {
+    public class WebPage {
+        public string Url;
+        public string Title;
+        public List<string> OutgoingUrls;
+        public TimeSpan CrawlTime;
+
+        public WebPage(string url, string title, List<string> OutgoingUrls, TimeSpan crawlTime) {
+            this.Url = url;
+            this.Title = title;
+            this.OutgoingUrls = OutgoingUrls;
+            this.CrawlTime = crawlTime;
+        }
+    }
+}
