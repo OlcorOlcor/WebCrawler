57d9d21d03073ec0db5bf4ce62ae20a2f456969a
Jan Simek <jan.simek.4242@gmail.com>
2023-08-02 01:09:23 +0200

    Add fetching data from site in Crawler.

--- WebCrawler/WebCrawler/Models/Crawler.cs
+++ WebCrawler/WebCrawler/Models/Crawler.cs
@@ -2,7 +2,14 @@
     public class Crawler {
         public List<WebPage> CrawlSite(WebPage webPage, string regex) {
             List<WebPage> foundWebPages = new();
-            throw new NotImplementedException();
+
+            //get data from server
+            //there should not be problem with size, because data are nomaly-sized webpages
+            using var client = new HttpClient();
+            Task<string> responseGet = client.GetStringAsync(webPage.Url);
+            responseGet.Wait();
+
+
         }
     }
 }
