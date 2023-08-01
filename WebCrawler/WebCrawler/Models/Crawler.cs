7567f763f9a509946acf17009e1c08cb24bd7983
Jan Simek <jan.simek.4242@gmail.com>
2023-08-02 01:58:58 +0200

    Add search and adding of webpages for next crawling.

--- WebCrawler/WebCrawler/Models/Crawler.cs
+++ WebCrawler/WebCrawler/Models/Crawler.cs
@@ -1,5 +1,8 @@
 ﻿namespace WebCrawler.Models {
     public class Crawler {
+        private static string RefString = "<a href=\"";
+        private static string QuatationMarksString = "\"";
+
         public List<WebPage> CrawlSite(WebPage webPage, string regex) {
             List<WebPage> foundWebPages = new();
 
@@ -8,8 +11,31 @@
             using var client = new HttpClient();
             Task<string> responseGet = client.GetStringAsync(webPage.Url);
             responseGet.Wait();
+            string pageString = responseGet.Result;
 
+            //search in data for reference
+            while (true) {
+                int indexOfFirstRef = pageString.IndexOf(RefString);
 
+                //end loop if there is non left
+                if(indexOfFirstRef == -1) {
+                    break;
+                }
+                else {
+                    //cut page up to current ref
+                    pageString = pageString.Substring(indexOfFirstRef + RefString.Length);
+                    //gets current url
+                    int indexOfNextQuatationMarks = pageString.IndexOf(QuatationMarksString);
+                    string currentUrl = pageString.Substring(0, indexOfNextQuatationMarks);
+                    //check if ref leads to walid url
+                    //TODO not checking properly
+                    Console.WriteLine(currentUrl);
+                    if (new Uri(currentUrl).Scheme == "http" || new Uri(currentUrl).Scheme == "https") {
+                        Console.WriteLine(currentUrl);
+                    }
+                }
+            }
         }
     }
 }
+
