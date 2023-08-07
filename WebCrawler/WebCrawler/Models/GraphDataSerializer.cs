69acafa561e608a629ecaba3ef83d20c23f96178
Jan Čelovský <celovskyhonza@gmail.com>
2023-08-07 16:30:58 +0200

    Implement Serializer

--- WebCrawler/WebCrawler/Models/GraphDataSerializer.cs
+++ WebCrawler/WebCrawler/Models/GraphDataSerializer.cs
@@ -1,22 +1,44 @@
-﻿namespace WebCrawler.Models {
-    public class GraphDataSerializer {
-        private WebsiteRecord WebsiteRecord { get; set; }
+﻿using System;
+using System.Text;
 
+namespace WebCrawler.Models {
+    public class GraphDataSerializer {
+        private WebsiteRecord _websiteRecord { get; set; }
+        private StringBuilder sb = new StringBuilder();
         public string SerializeRecord(WebsiteRecord record) {
             WebsiteRecord websiteRecord = record;
+            sb.Append("{[");
+            int executionNumber = 0;
 
-            foreach(var execution in record.RunningExecutions) {
-
+            SerializeExecution(websiteRecord.LastFinishedExecution, executionNumber++);
+            foreach (var execution in record.RunningExecutions) {
+                SerializeExecution(execution, executionNumber);
+                executionNumber++;
             }
+            sb.Append("]}");
+            return sb.ToString();
         }
-        private string SerializeExecution() {
-
+        private void SerializeExecution(Execution execution, int executionNumber) {
+            WebPage[] webPages = execution.pages.ToArray();
+            sb.Append($"\"Execution{executionNumber}\": {{ ");
+            SerializeNodes(webPages);
+            SerializeLinks(webPages);
         }
-        private string SerializeVertices() {
-
+        private void SerializeNodes(WebPage[] webPages) {
+            sb.Append("\"nodes\": [");
+            foreach (var page in webPages) {
+                sb.Append($"{{\"id\": {page.Url}, \"group\": 1}},");
+            }
+            sb.Append("],");
         }
-        private string SerializeEdges() {
-
+        private void SerializeLinks(WebPage[] webPages) {
+            sb.Append("\"links\": [");
+            foreach (var webPage in webPages) {
+                foreach (var link in webPage.OutgoingUrls) {
+                    sb.Append($"{{\"source\": {webPage.Url}, \"target\": {link}, \"value\": 1}},");
+                }
+            }
+            sb.Append("]");
         }
     }
 }
