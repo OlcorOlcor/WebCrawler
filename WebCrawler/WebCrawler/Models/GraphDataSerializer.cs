6ae1a6a799f2225e8a92cd7af7186f3c078e748a
Jan Simek <jan.simek.4242@gmail.com>
2023-08-07 15:37:55 +0200

    Create serializer ,method structure without implementation.

---
+++ WebCrawler/WebCrawler/Models/GraphDataSerializer.cs
@@ -0,0 +1,22 @@
+﻿namespace WebCrawler.Models {
+    public class GraphDataSerializer {
+        private WebsiteRecord WebsiteRecord { get; set; }
+
+        public string SerializeRecord(WebsiteRecord record) {
+            WebsiteRecord websiteRecord = record;
+
+            foreach(var execution in record.RunningExecutions) {
+
+            }
+        }
+        private string SerializeExecution() {
+
+        }
+        private string SerializeVertices() {
+
+        }
+        private string SerializeEdges() {
+
+        }
+    }
+}
