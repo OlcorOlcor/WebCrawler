2585e7aade2a55ac4711197bada12e4b0c46466e
Jan Čelovský <celovskyhonza@gmail.com>
2023-09-09 17:19:01 +0200

    Add Serializer for Executions

---
+++ WebCrawler/WebCrawler/Models/ExecutionSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace WebCrawler.Models {
+    public class ExecutionSerializer {
+        private StringBuilder sb = new StringBuilder();
+        private void SerializeExecution(Execution execution, int recordId) {
+            sb.Append("{");
+            sb.Append($"\"RecordId\": {recordId},");
+            sb.Append($"\"Time\": {execution.ExecutionTime},");
+            sb.Append($"\"Status\": FINISHED"); //TODO: Change to actual status once implemented
+            sb.Append("}");
+        }
+        public string SerializeLatestExecutions(List<WebsiteRecord> records) {
+            sb.Append("{ \"Executions\": [");
+            foreach (WebsiteRecord record in records) {
+                SerializeExecution(record.LastFinishedExecution, record.Id);
+            }
+            sb.Append("]}");
+            return sb.ToString();
+        }
+    }
+}
