3a98b3fc8cf46b0a9cafacc94daa855e53a22928
Jan Čelovský <celovskyhonza@gmail.com>
2023-09-12 18:26:16 +0200

    Add thead

--- WebCrawler/WebCrawler/wwwroot/js/WebRecordTable.svelte
+++ WebCrawler/WebCrawler/wwwroot/js/WebRecordTable.svelte
@@ -2,5 +2,27 @@
 
 <script>
 
+
+
 </script>
 
+<div class="list">
+    <h3>List of current Website Records</h3>
+    <table class="table table-striped" id="update">
+        <thead>
+            <tr>
+                <td>Url</td>
+                <td>Regex</td>
+                <td>Periodicity</td>
+                <td>Label</td>
+                <td>Time of last executions</td>
+                <td>Status of last executions</td>
+                <td>Tags</td>
+                <td>Crawl now</td>
+            </tr>
+        </thead>
+        <tbody>
+            <!--foreach-->      
+        </tbody>
+    </table>  
+</div>
