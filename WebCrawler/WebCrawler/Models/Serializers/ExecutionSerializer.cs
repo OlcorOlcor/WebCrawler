6bd26f4b63e45e615e9f08bf24c742d5c91e6030
Jan Simek <jan.simek.4242@gmail.com>
2023-09-13 15:22:50 +0200

    Add serialization of Executions for table

--- WebCrawler/WebCrawler/Models/Serializers/ExecutionSerializer.cs
+++ WebCrawler/WebCrawler/Models/Serializers/ExecutionSerializer.cs
@@ -1,22 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
 using System.Text;
 
 namespace WebCrawler.Models.Serializers {
     public class ExecutionSerializer : ISerializer<Execution> {
         private StringBuilder? sb;
 
+        public string SerializeAllExecutions(WebsiteRecordRepository repository) {
+            sb = new StringBuilder();
+            sb.Append("{\"Executions\":[");
+            bool isFirst = true;
+            foreach (var record in repository.GetAllRecords()) {
+                foreach (Execution execution in record.GetAllExecutions()) {
+                    if (isFirst) {
+                        sb.Append(Serialize(execution));
+                        isFirst = false;
+                    }
+                    else {
+                        sb.Append("," + Serialize(execution));
+                    }
+                }
+            }
+            sb.Append("]}");
+            return sb.ToString();
+        }
+
         public string Serialize(Execution execution) {
             //TODO: Serialize Execution for Execution table (maybe could merge/reuse SerializeExecutionForWebsiteRecordTable method
             throw new NotImplementedException();
         }
-        private void SerializeExecutionForWebsiteRecordTable(Execution execution, int recordId) {
+        private void SerializeExecutionForWebsiteRecordTable(Execution execution, int recordId, string recordLabel) {
             if (sb is null) {
                 return;
             }
             sb.Append("{");
-            sb.Append($"\"RecordId\": {recordId},");
+            sb.Append($"\"RecordId\": {recordId},"); //probably should not be displayed for user
+            sb.Append($"\"RecordLabel\": {recordLabel},");
             sb.Append($"\"Time\": \"{execution.ExecutionTime}\",");
-            sb.Append($"\"Status\": \"{execution.Status}\"");
+            sb.Append($"\"Status\": {execution.Status.ToString()},");
+            sb.Append($"\"NumberOfSitesCrawled\": {execution.pages.Count}");
             sb.Append("}");
         }
         public string SerializeLatestExecutions(IList<WebsiteRecord> records) {
@@ -28,7 +50,7 @@ namespace WebCrawler.Models.Serializers {
                     if (!firstRecord) {
                         sb.Append(",");
                     }
-                    SerializeExecutionForWebsiteRecordTable(record.LastFinishedExecution, record.Id);
+                    SerializeExecutionForWebsiteRecordTable(record.LastFinishedExecution, record.Id, record.Label!);
                 }
             }
             sb.Append("]}");
