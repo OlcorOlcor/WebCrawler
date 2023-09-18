9792baae2be427cb037df0b91ad50dc202e09f71
Jan Čelovský <celovskyhonza@gmail.com>
2023-09-18 16:58:17 +0200

    change color of delete button

--- WebCrawler/WebCrawler/wwwroot/js/WebRecordTable.svelte
+++ WebCrawler/WebCrawler/wwwroot/js/WebRecordTable.svelte
@@ -148,7 +148,7 @@ function deleteRecord(record){
                     <td><button type="button" class="btn btn-primary" on:click={startNewExecution(record.Id)}>Start New Execution</button></td>
                     <td><button type="button" class="btn btn-primary" on:click={requestExecutionFilter(record.Id)}>Show Related Executions</button></td>
                     <td><button type="button" class="tbn btn-primary" on:click={showGraph(record.Id)}>Show Graph</button></td>
-                    <td><button type="button" class="btn btn-primary" on:click={deleteWebSiteRecord(record.Id)}>Delete Record</button></td>
+                    <td><button type="button" class="btn btn-danger" on:click={deleteWebSiteRecord(record.Id)}>Delete Record</button></td>
                 </tr>
             {/each}
         </tbody>
